ad6c11b0b933414d8a41afd2023be16059a2c147
Fandy Hidayat <fandy15ti@mahasiswa.pcr.ac.id>
2019-12-13 14:24:18 +0700

    update

--- src/Controller/BlogApiController.php
+++ src/Controller/BlogApiController.php
@@ -8,6 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogApiController extends AbstractController
 {
     /**
-     * @Route("/api/posts/{id}", )
+     * @Route("/api/posts/{id}", methods={"GET","HEAD"})
      */
+    public function show(int $id)
+    {
+        // ... return a JSON response with the post
+    }
+
+    /**
+     * @Route("/api/posts/{id}", methods={"PUT"})
+     */
+    public function edit(int $id)
+    {
+        // ... edit a post
+    }
 }
---
+++ src/Controller/TaskController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+
+class TaskController extends AbstractController
+{
+    public function new(Request $request)
+    {
+        // creates a task object and initializes some data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        // ...
+    }
+}
